46778383bb535bf628b8bb121711ed786ee9facc
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-11 07:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file.
-function lowerCaseDrivers(input, function(text) {return input.toLowerCase})
+function lowerCaseDrivers(map(input, function(text) {return input.toLowerCase}))
 
 
 }
