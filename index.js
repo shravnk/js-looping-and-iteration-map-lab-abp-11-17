e48e18504d604ac6f523584a981596ff4f1adea3
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-11 08:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function lowerCaseDrivers(input) {
   return input.map(function (text) { return text.toLowerCase(); });
 }
+function nameToAttributes(input) {
+  return input.map(function (text) { return object.assign({},text,{firstName: text.split(0)) });}
+  
+  
+}
